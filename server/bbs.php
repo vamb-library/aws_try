47486f8bc6ef25dd879632f39319693c4293061d
tkakui <s203135@stn.nagaokaut.ac.jp>
2021-09-18 21:23:08 +0900

    システムメッセージの表示を追加

--- server/bbs.php
+++ server/bbs.php
@@ -1,10 +1,46 @@
+<!DOCTYPE html>
 <html>
-
 <head>
   <title>vamb掲示板_練習</title>
   <style type=text/css>
     div#header{background-color:#e0ffff;}
+  
+    .success_message {
+      margin: 20px;
+      /*margin-bottom: 20px;*/
+      padding: 10px;
+      /*color: #48b400;*/
+      color: #008b8b;
+      border-radius: 10px;
+      border: 2px solid #00b3b3;
+    }
+
+    .error_message {
+      margin-top: 10px;
+      margin-left: 20px;
+      margin-right: 20px;
+      padding: 10px;
+      color: #ef072d;
+      /*list-style-type: none;*/
+      border-radius: 10px;
+      border: 1px solid #ff5f79;
+    }
+
+    .delete_message {
+      margin: 20px;
+      padding: 10px;
+      color: #ff0000;
+      border: 3px double #ff4500;
+      border-radius: 5px;
+    }
+
+    .success_message,
+    .error_message {
+    font-size: 86%;
+    line-height: 1.6em;
+    }
   </style>
+  
   <!-- bootstrap CDN -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -15,7 +51,7 @@
 <div id="header">
 
 <h1>vamb@ch</h1>
-<h4 style="color : ff8c00">ちょっと頑張ってみよう_k</h4>
+<h4 style="color: #ff8c00">ちょっと頑張ってみよう_</h4>
 
 <h2>投稿するところ</h2>
 <form action="bbs.php" method="post" role="form">
@@ -47,29 +83,34 @@ try{
   $error_message[] = $ex->getMessage();
 }
 
-//トランザクション開始
-$pdo->beginTransaction();
-try{
-  // 変数の設定
-  $content = $_POST["content"];
-  $u_name = $_POST["user_name"];
-  $delete_id = $_POST["delete_id"];
+// 変数の設定
+$content = $_POST["content"];
+$u_name = $_POST["user_name"];
+$delete_id = $_POST["delete_id"];
+$error_message = array();   //エラー文をここに格納
+$res = null;
 
-  //投稿ボタンが押されたらデータベースへのデータの挿入開始
-  if( isset($_POST["submit_btn"]) ){
-    //名前が空ならエラー文
-    if(empty($u_name)){
-      $error_message[] = "名前を入力せよ";
-    }
+//投稿ボタンが押されたらデータベースへのデータの挿入開始
+if( isset($_POST["submit_btn"]) ){
+  //名前が空ならエラー文を記録
+  if(empty($u_name)){
+    $error_message[] = "名前を入力せよ";
+    //echo "名前を入力しよ<br />";
+  }
 
-    //投稿内容が空ならエラー文
-    if(empty($content)){
-      $error_message[] = "投稿内容を入力せよ";
-    }
+  //投稿内容が空ならエラー文
+  if(empty($content)){
+    $error_message[] = "投稿内容を入力せよ";
+    //echo "投稿内容を入力せよ<br />";
+  }
 
-    if(!empty($error_message)){ //エラー文がなければ
-      //var_dump($_POST);
-    
+  if(empty($error_message)){ //エラー文がなければ
+    //var_dump($_POST);
+
+    try{
+      //トランザクション開始
+      $pdo->beginTransaction();
+
       //データベースへ挿入
       $sql  = "INSERT INTO bbs (content, updated_at, user_name) VALUES (:content, NOW(), :user_name);";
       $stmt = $pdo->prepare($sql);
@@ -79,39 +120,92 @@ try{
 
       //コミット
       $res = $pdo->commit();
-
-      if($res){
-        $suc_meg = "投稿されました。一番下";
-      }else{
-        $error_message[] = "書き込みに失敗";
-      }
-    }else{  //エラー分があれば
-      echo "投稿に失敗";
+      //$res = 0;
+    }catch(Exception $ex){ //コミットまで何らかのエラーがあればロールバック
+      $pdo->rollBack();
+      echo "失敗したrollback". $ex->getMessage();
     }
+
+    if($res){
+      //データベースに挿入できたら成功メッセージを格納
+      $suc_meg = "投稿されました。一番下";
+      //echo "投稿されたよ";
+    }else{
+      $error_message[] = "投稿内容の書き込みに失敗しました";
+      //echo "書き込みに失敗";
+    }
+  }else{  //エラー分があれば
+    $error_message[] = "投稿に失敗しました";
+    //echo "投稿に失敗しました。";
   }
-} catch(Exception $ex){
-  $pdo->rollBack();
 }
 
-
-
 // データベースのデータの削除
-$sql = "DELETE FROM bbs WHERE ip = :delete_id;";
-$stmt = $pdo->prepare($sql);
-$stmt -> bindValue(":delete_id", $delete_id, PDO::PARAM_STR);
-$stmt -> execute();
+try{
+  $pdo -> beginTransaction();
+
+  $sql = "DELETE FROM bbs WHERE ip = :delete_id;";
+  $stmt = $pdo->prepare($sql);
+  $stmt -> bindValue(":delete_id", $delete_id, PDO::PARAM_STR);
+  $stmt -> execute();
+
+  $res = $pdo -> commit();
+  //$res = 0;
+}catch(Exception $ex){
+  $pdo -> rollBack();
+  $error_message[] = $ex -> getMessage();
+  echo "削除に失敗rollBack". $ex -> getMessage();
+}
+if($res){
+  $del_meg = "投稿が削除されました。: ". $delete_id. "<br /> この事象は管理者に報告され、
+  操作元を特定します。<br /> って書いたらビビるよなぁ。";
+}else{
+  $error_message[] = "削除に失敗しました。";
+}
 
 // データベースからのデータの取得
 $sql = "SELECT * FROM bbs ORDER BY updated_at $order;";
 $stmt = $pdo->prepare($sql);
 $stmt -> execute();
 
-// 取得したデータをテーブルで表示
+?>
 
-//投稿メッセージが削除されたら削除メッセージを表示
-if($delete_id != NULL){
-  echo "投稿が削除されました。: ".$delete_id;
+<?php
+//システムメッセージ
+
+//投稿メッセージがdbに正常に入力されたときのメッセージ
+if(!empty($suc_meg)){
+?>
+<p class = "success_message"><?php echo $suc_meg; ?></p>
+<?php
 }
+//何らかのエラーを検出したときのメッセージ
+if(!empty($error_message)){
+  foreach($error_message as $err_value){?>
+    <p class = "error_message">
+      <?php echo $err_value; ?>
+    </p>
+  <?php
+  }
+  ?>
+<!-- <ul class = "error_message">
+  ?php foreach( $error_message as $err_value){?>
+    <li> ?php echo $err_value; ?> </li>
+  ?php
+  }
+  ?>
+</ul> -->
+<?php
+}
+
+//投稿メッセージが削除されたときのメッセージ
+if(!empty($delete_id)){
+?>  
+<p class = "delete_message"> <?php echo $del_meg ?></p>
+<?php
+}
+
+// 取得したデータをテーブルで表示
 ?>
 
 <h2>投稿リスト</h2>
