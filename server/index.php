342feed4e209da50a52359072113eeeb9b7cf3ff
tkakui <s203135@stn.nagaokaut.ac.jp>
2021-10-01 17:27:03 +0900

    ホームページのデザインを改良かっこよくなったけどほぼサンプル通り

--- server/index.php
+++ server/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
     <head>
         <title>vamb_page</title>
         <!-- bootstrap 5.x -->
@@ -24,16 +24,50 @@
 
         }
         </style>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div id="head">
-                <h1>vamb_page</h1>
-                <a class="btn btn-link" href="http://54.150.237.25/bbs/" role="button">掲示板</a>
-                <a class="btn btn-link" href="http://54.150.237.25/sample_game/" role="button">サンプルゲーム</a>
-            </div>
 
-        </div>
+        <!-- Custom styles for this template -->
+        <link href="cover.css" rel="stylesheet">
+
+    </head>
+
+    <body class="d-flex h-100 text-center text-white bg-dark">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Vamb_page</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="http://54.150.237.25/">Home</a>
+        <!--<a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>-->
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <h1>Welcome to vamb_page</h1>
+    <p class="lead">Hello! HAHAHAHAHAAHA!!　どうもこんにちわ、ウェブサイトづくり頑張り中です。
+        下のは掲示板とサンプルゲームの置き場所です。ちなみにこのデザインはbootstrapのサンプルから
+        持ってきたものです。すごいですね。
+    </p>
+    <p class="lead">
+      <a href="http://54.150.237.25/bbs/" class="btn btn-lg btn-secondary fw-bold border-white bg-white">掲示板</a>
+      <a href="http://54.150.237.25/sample_game/" class="btn btn-lg btn-secondary fw-bold border-white bg-white">sample_game</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  </footer>
+</div>
+<!--
+<div class="container-fluid">
+    <div id="head">
+        <h1>vamb_page</h1>
+        <a class="btn btn-link" href="http://54.150.237.25/bbs/" role="button">掲示板</a>
+        <a class="btn btn-link" href="http://54.150.237.25/sample_game/" role="button">サンプルゲーム</a>
+    </div>
+</div>
+    -->
     </body>
 </html>
 
